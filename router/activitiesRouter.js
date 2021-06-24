c0107a53188308066be84567f26d25852808ebbd
voquocminh <19521857@gm.uit.edu.vn>
2021-06-24 23:23:20 +0700

    get by userID

--- router/activitiesRouter.js
+++ router/activitiesRouter.js
@@ -19,16 +19,16 @@ router.get('/get', async function(req, res){
 
 
 //get all activities by userID
-// router.get('/userID=:userID', async function(req, res){
-//     var id = req.params.userID.toString();
-//     try{
-//         var activities = await Activities.find({userID: id});
-//         res.send(activities);
-//     }catch(err){
-//         res.send(err)
-//     }
-//     console.log(res)
-// });
+router.get('/userID=:userID', async function(req, res){
+    var id = req.params.userID.toString();
+    try{
+        var activities = await Activities.find({userID: id});
+        res.send(activities);
+    }catch(err){
+        res.send(err)
+    }
+    console.log(res)
+});
 
 //get activity by id
 router.get('/id=:id', async function(req, res){
@@ -42,27 +42,27 @@ router.get('/id=:id', async function(req, res){
 });
 
 //get all activities by 1 date //yyyy-mm-dd
-    // router.get('/date=:date', async function(req, res){
-    //     var d = req.params.date;
-    //     var x="T00:00:00.000Z";
-    //     var y="T23:59:59.000Z";
-    //     var d1= d.concat(x);
-    //     var d2= d.concat(y);
-    //     var activities = await Activities.find(
-    //         {date:
-    //             {
-    //             $gte: d1,
-    //             $lt: d2
-    //             }}
-    //     )
-    //     if(activities){
-    //         res.send(activities)
-    //     }else{
-    //         res.status(500).send("bad server")
-    //     }
-    // })
+    router.get('/date=:date', async function(req, res){
+        var d = req.params.date;
+        var x="T00:00:00.000Z";
+        var y="T23:59:59.000Z";
+        var d1= d.concat(x);
+        var d2= d.concat(y);
+        var activities = await Activities.find(
+            {date:
+                {
+                $gte: d1,
+                $lt: d2
+                }}
+        )
+        if(activities){
+            res.send(activities)
+        }else{
+            res.status(500).send("bad server")
+        }
+    })
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////get activitities by 1 user 1 date 
     router.get('/userID/:userID/date/:date', async function(req, res){
         var userID= req.params.userID;
         var d = req.params.date;
@@ -107,6 +107,31 @@ router.get('/month=:month', async function(req, res){
         res.status(500).send("bad server")
     }
 })
+////////////////////////////////////////////////////////////////////get activitis by 1 userID  1 month
+router.get('/userID/:userID/month/:month', async function(req, res){
+    var m = req.params.month;
+    var userID=req.params.userID;
+    var x="-01T00:00:00.000Z";
+    var y="-31T23:59:59.000Z";
+    var m1= m.concat(x);
+    var m2= m.concat(y);
+    var activities = await Activities.find({
+        userID:userID,
+        date:{
+            $gte:m1,
+            $lt:m2
+        }
+    },function(err, activities)
+    {
+        if(activities){
+            res.send(activities)
+        }else{
+            res.status(500).send(err)
+        }
+    })        
+})
+
+
 //get activities a this week 
 router.get('/thisweek', async function(req,res){
     var thu= moment().format('dddd') //wednesday
@@ -161,6 +186,64 @@ router.get('/thisweek', async function(req,res){
     }else{
         res.status(500).send("bad server")
     }
+})
+////////////////////////////////////////////////////get activities this week by 1 userID 
+router.get('/userID/:userID/thisweek', async function(req,res){
+    var userID=req.params.userID;
+    var thu= moment().format('dddd') //thứ (Monday, Tuesday,...)
+    console.log(thu);
+
+    var day=0;
+    switch(thu){
+        case "Monday":
+            day= 6;
+            break;
+        case "Tuesday":
+            day= 5;
+            break;
+        case "Wednesday":
+            day= 4;
+            break;
+        case "Thursday":
+            day= 3;
+            break;
+        case "Friday":
+            day= 2;
+            break;
+        case "Saturday":
+            day= 1;
+            break;
+        case "Sunday":
+            day= 0;
+            break;
+        default: day=0;
+    }
+    console.log(day);
+
+    var d1=moment().subtract((6-day), 'days').format(); 
+    var d2=moment().add(day, 'days').format(); 
+    // console.log(d1)
+    // console.log(d2)
+    var d11= d1.split('T')
+    d1=d11[0]+"T00:00:00.000Z";
+    var d22= d2.split('T')
+    d2=d22[0]+"T23:59:59.000Z";
+    // console.log(d1)
+    // console.log(d2)
+    var activities = await Activities.find({
+        userID:userID,
+        date:{
+            $gte:d1,
+            $lt:d2
+        }
+    },function(err, activities)
+    {
+        if(activities){
+            res.send(activities)
+        }else{
+            res.status(500).send(err)
+        }
+    })        
 })
 
 //post 1 activity
