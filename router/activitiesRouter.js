c1f0c317aa25c28db5d0c400bd74a13866f29e38
voquocminh <19521857@gm.uit.edu.vn>
2021-07-03 10:56:28 +0700

    update lần cuồi lần 2

--- router/activitiesRouter.js
+++ router/activitiesRouter.js
@@ -22,7 +22,7 @@ router.get('/get', async function(req, res){
 router.get('/userID/:userID', async function(req, res){
     var id = req.params.userID.toString();
     try{
-        var activities = await Activities.find({userID: id}).sort({'date':-1}).limit(20)
+        var activities = await Activities.find({userID: id}).sort({'date':-1}).limit(7)
         res.send(activities);
     }catch(err){
         res.send(err)
